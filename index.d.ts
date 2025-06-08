52024098b7f04fb0f7b8f8fdef71a13775a0f99a
wow <151538495+IMJNMHTODRLA@users.noreply.github.com>
2025-06-08 20:56:12 +0900

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
-declare function toBytes(size: string, Separator: boolean, IsNumber: boolean): string | number;
-declare function toReadBytes(size: string, Separator: boolean, IsNumber: boolean): string | number;
+declare function toBytes(size: string, Separator: boolean, IsNumber: boolean): string | number
+declare function toReadBytes(size: string, Separator: boolean, IsNumber: boolean): string | number
+declare function toSetUnit(size: string, Separator: boolean, IsNumber: boolean, unit: string): string | number
 
 declare namespace toBytes{}
 declare namespace toReadBytes{}
+declare namespace toSetUnit{}
 
-export { toBytes, toReadBytes };
+export { toBytes, toReadBytes, toSetUnit };
