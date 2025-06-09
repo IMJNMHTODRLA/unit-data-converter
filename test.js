7f6f555cf5555ff89febf58d35cf1424b63bb341
wow <151538495+IMJNMHTODRLA@users.noreply.github.com>
2025-06-09 20:03:38 +0900

    Update Version 1.1.0
    Update details available in README.md

--- test.js
+++ test.js
@@ -1,33 +1,17 @@
-const { toBytes, toReadBytes } = require('./');
+const { toBytes } = require('./');
 
 /** toBytes Function */
 
-console.log(toBytes('5GB', false, true));
-//5,368,709,120(int)
+console.log(toBytes('5.1GB', false, true));
+//5,476,083,302.4(int)
 
-console.log(toBytes('5GB', true, false));
-//5,368,709,120 B(string)
+console.log(toBytes('5.1GB', true, false));
+//5,476,083,302.4 B(string)
 
-console.log(toBytes('50MB', false, false));
-//5,368,709,120(string)
+console.log(toBytes('5.1GB', false, false));
+//5,476,083,302.4(string)
 
-console.log(toBytes('50MB', true, true));
-//Error: Is string vaule
-
-/** End */
-
-/** toReadBytes */
-
-console.log(toReadBytes('52428800 B', false, true));
-//50(int)
-
-console.log(toReadBytes('52428800 B', true, false));
-//50 MB(string)
-
-console.log(toReadBytes('52428800 B', false, false));
-//50(string)
-
-console.log(toReadBytes('52428800 B', true, true));
+console.log(toBytes('5.1GB', true, true));
 //Error: Is string vaule
 
 /** End */
